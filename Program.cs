86b809dc73b4658c480b32bc29d0e9e1f3cd6b05
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-09 22:40:59 +0530

    Adding program for data management of rice, pulse and wheat

--- Program.cs
+++ Program.cs
@@ -12,6 +12,52 @@ namespace InventoryManagement
 
             Console.WriteLine("Wellcome to Object Oriented Programs");
 
+            FetchInventoryDetails fetchInventoryDetails = new FetchInventoryDetails();
+
+
+            string filepath = "D:\\repo179batch\\InventeryManagment\\InventeryManagment\\Data.json";
+
+            Rice data = fetchInventoryDetails.Read(filepath);
+
+            Console.WriteLine("Types of Rice");
+            Console.WriteLine("--------------");
+
+            for (int i = 0; i < data.typesofRice.Count; i++)
+            {
+
+                Console.WriteLine(data.typesofRice[i].name);
+                Console.WriteLine(data.typesofRice[i].price);
+                Console.WriteLine(data.typesofRice[i].weight);
+
+                Console.WriteLine("--------------");
+            }
+
+            Console.WriteLine("Types of Pulses");
+            Console.WriteLine("--------------");
+
+            for (int i = 0; i < data.typesofPulses.Count; i++)
+            {
+
+                Console.WriteLine(data.typesofPulses[i].name);
+                Console.WriteLine(data.typesofPulses[i].price);
+                Console.WriteLine(data.typesofPulses[i].weight);
+
+                Console.WriteLine("--------------");
+            }
+
+            Console.WriteLine("Types of Wheat");
+            Console.WriteLine("--------------");
+
+            for (int i = 0; i < data.typesofWheat.Count; i++)
+            {
+
+                Console.WriteLine(data.typesofWheat[i].name);
+                Console.WriteLine(data.typesofWheat[i].price);
+                Console.WriteLine(data.typesofWheat[i].weight);
+
+                Console.WriteLine("--------------");
+            }
+
         }
     }
 }
