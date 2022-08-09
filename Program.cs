a77a29e7719e5c7404a469e78f848af6bacc8c64
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-09 22:27:42 +0530

    Grreting Messsage

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using InventeryManagment;
+
+namespace InventoryManagement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Wellcome to Object Oriented Programs");
+
+        }
+    }
+}
