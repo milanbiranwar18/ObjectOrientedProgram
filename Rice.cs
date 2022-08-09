d0c292af73292bc3c3eddbab5cb848e2d89366d5
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-09 22:35:43 +0530

    Program for types of Rice, Pulses and Wheats

---
+++ Rice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventeryManagment
+{
+    public class Rice
+    {
+        public List<TypesofRice> typesofRice;
+        public List<TypesofPulses> typesofPulses;
+        public List<TypesofWheat> typesofWheat;
+    }
+
+    public class TypesofRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+
+    public class TypesofPulses
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+
+    public class TypesofWheat
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+
+
+
+}
