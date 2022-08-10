c49977866071c3f54385db2d33cf83aa84836741
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-10 15:51:22 +0530

    Adding program for Inventory management problem

--- Program.cs
+++ Program.cs
@@ -28,6 +28,9 @@ namespace InventoryManagement
                 Console.WriteLine(data.typesofRice[i].name);
                 Console.WriteLine(data.typesofRice[i].price);
                 Console.WriteLine(data.typesofRice[i].weight);
+                int total = data.typesofRice[i].price * data.typesofRice[i].weight;
+                Console.WriteLine("The price of " + data.typesofRice[i].name + " for " + data.typesofRice[i].weight + " Kg " + total);
+
 
                 Console.WriteLine("--------------");
             }
@@ -41,9 +44,11 @@ namespace InventoryManagement
                 Console.WriteLine(data.typesofPulses[i].name);
                 Console.WriteLine(data.typesofPulses[i].price);
                 Console.WriteLine(data.typesofPulses[i].weight);
+                int total = data.typesofPulses[i].price * data.typesofPulses[i].weight;
+                Console.WriteLine("The price of " + data.typesofPulses[i].name + " for " +data.typesofPulses[i].weight + " Kg "+ total);
 
                 Console.WriteLine("--------------");
-            }
+           }
 
             Console.WriteLine("Types of Wheat");
             Console.WriteLine("--------------");
@@ -54,6 +59,9 @@ namespace InventoryManagement
                 Console.WriteLine(data.typesofWheat[i].name);
                 Console.WriteLine(data.typesofWheat[i].price);
                 Console.WriteLine(data.typesofWheat[i].weight);
+                int total = data.typesofWheat[i].price * data.typesofWheat[i].weight;
+                Console.WriteLine("The price of " + data.typesofWheat[i].name + " for " + data.typesofWheat[i].weight + " Kg " + total);
+
 
                 Console.WriteLine("--------------");
             }
